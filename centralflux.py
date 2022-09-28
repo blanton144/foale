d056510c129a053324133b9a2a68c47b9e624557
Janelle Sy <u6044257@notchpeak19.int.chpc.utah.edu>
2022-09-28 11:09:07 -0600

    Updated to calculate variance and standard deviation in flux measurements

--- centralflux.py
+++ centralflux.py
@@ -1,4 +1,7 @@
 #!/usr/bin/env python
+# coding: utf-8
+
+#!/usr/bin/env python
 
 import numpy as np
 import matplotlib.pyplot as plt
@@ -14,12 +17,12 @@ summary_file = summary_file.format(v=version)
 f = fitsio.read(summary_file)
 header = fitsio.read(summary_file, header = True)
 
-# extract plateifu for each galaxy
-MNSA_plateifu = f['plateifu']
-
 # initialize Pipe3D data
 pipe3D = fitsio.FITS('SDSS17Pipe3D_v3_1_1.fits')
 pipe3D_galproperties = pipe3D[1]
+
+# extract plateifu for each galaxy
+MNSA_plateifu = f['plateifu']
 
 # match the plateifus from Pipe3D with MNSA
 pipe3D_plateifu = pipe3D_galproperties['plateifu'][:]
@@ -47,8 +50,8 @@ def central_flux(p_IFU):
     if(success is True):
         wave = m.cube['WAVE'].read()
         flux = m.cube['FLUX'].read()
-        PSF = m.cube['RPSF'].read()
-
+        PSF = m.cube['RPSF'].read()        
+        
         # Read in the emission line measurements (Gaussian fit)
         # This is a big 3D array, with one 2D image per line.
         gflux = m.maps['EMLINE_GFLUX'].read()
@@ -70,7 +73,17 @@ def central_flux(p_IFU):
         OI = gflux[OI_indx, :, :]
         OIII = gflux[OIII_indx, :, :]
         NII = gflux[NII_indx, :, :]
-
+        
+        # Inverse variance maps for each line       
+        gflux_ivar = m.maps['EMLINE_GFLUX_IVAR'].read()
+        halpha_ivar = gflux_ivar[halpha_indx, :, :]
+        hbeta_ivar = gflux_ivar[hbeta_indx, :, :]
+        SII_6718_ivar = gflux_ivar[SII_6718_indx, :, :]
+        SII_6732_ivar = gflux_ivar[SII_6732_indx,:,:]
+        OI_ivar = gflux_ivar[OI_indx, :, :]
+        OIII_ivar = gflux_ivar[OIII_indx,:,:]
+        NII_ivar = gflux_ivar[NII_indx,:,:]
+        
         # masked DAP maps
         gfluxmask = m.maps['EMLINE_GFLUX_MASK'].read()
         halpha_mask = np.ma.array(halpha, mask = gfluxmask[halpha_indx,:,:])
@@ -85,50 +98,81 @@ def central_flux(p_IFU):
         # bit number = 0 --> no coverage in cube
         m = (halpha_mask == 0) | (hbeta_mask == 0 ) | (SII_6718_mask == 0) |(SII_6732_mask ==0) | (OI_mask == 0) |(OIII_mask == 0) | (NII_mask == 0) |(halpha <= 0) | (hbeta <= 0) |(SII_6718 <= 0 ) | (SII_6732 <= 0 ) |(OI <= 0) |(OIII <= 0) |(NII <= 0)
 
-        # apply mask to each emission flux measurement array
+        # apply mask to each emission flux array, ivar, and the PSF
         ma_nz_halpha = np.ma.array(halpha, mask = m)
         ma_nz_hbeta = np.ma.array(hbeta, mask = m)
         ma_nz_SII_6718 = np.ma.array(SII_6718, mask = m)
         ma_nz_SII_6732 = np.ma.array(SII_6732, mask = m)
+        ma_nz_SII =  ma_nz_SII_6718 + ma_nz_SII_6732
         ma_nz_OI = np.ma.array(OI, mask = m)
         ma_nz_OIII = np.ma.array(OIII, mask = m)
         ma_nz_NII = np.ma.array(NII, mask = m)
-
-        # array of the sum of the SII doublet
-        ma_nz_SII =  ma_nz_SII_6718 + ma_nz_SII_6732
-
+        
+        ma_nz_SII_6718_ivar = np.ma.array(SII_6718_ivar, mask = m)
+        ma_nz_SII_6732_ivar = np.ma.array(SII_6732_ivar, mask = m)
+        ma_nz_SII_ivar = ma_nz_SII_6718_ivar + ma_nz_SII_6732_ivar
+        ma_nz_OI_ivar = np.ma.array(OI_ivar, mask = m)
+        ma_nz_OIII_ivar = np.ma.array(OIII_ivar, mask = m)
+        ma_nz_NII_ivar = np.ma.array(NII_ivar, mask = m)
+        
+        # variance
+        ma_nz_PSF = np.ma.array(PSF, mask = m)
+        
+        w = ma_nz_PSF / (ma_nz_PSF**2).sum()
+        
+        SII_variance = np.sum(w**2 / ma_nz_SII_ivar)
+        OI_variance = np.sum(w**2 / ma_nz_OI_ivar)
+        OIII_variance = np.sum(w**2 / ma_nz_OIII_ivar)
+        NII_variance = np.sum(w**2 / ma_nz_NII_ivar)
+        
+        # standard deviation
+        SII_sigma = np.sqrt(SII_variance)
+        OI_sigma = np.sqrt(OI_variance)
+        OIII_sigma = np.sqrt(OIII_variance)
+        NII_sigma = np.sqrt(NII_variance)
+        
         # central flux of each emission line
-        halpha_cf = (ma_nz_halpha*PSF).sum() / (PSF**2).sum()
-        hbeta_cf = (ma_nz_hbeta*PSF).sum() / (PSF**2).sum()
-        SII_cf = (ma_nz_SII*PSF).sum() / (PSF**2).sum()
-        OI_cf = (ma_nz_OI*PSF).sum() / (PSF**2).sum()
-        OIII_cf = (ma_nz_OIII*PSF).sum() / (PSF**2).sum()
-        NII_cf = (ma_nz_NII*PSF).sum() / (PSF**2).sum()
+        halpha_cf = (ma_nz_halpha*ma_nz_PSF).sum() / (ma_nz_PSF**2).sum()
+        hbeta_cf = (ma_nz_hbeta*ma_nz_PSF).sum() / (ma_nz_PSF**2).sum()
+        SII_cf = (ma_nz_SII*ma_nz_PSF).sum() / (ma_nz_PSF**2).sum()
+        OI_cf = (ma_nz_OI*ma_nz_PSF).sum() / (ma_nz_PSF**2).sum()
+        OIII_cf = (ma_nz_OIII*ma_nz_PSF).sum() / (ma_nz_PSF**2).sum()
+        NII_cf = (ma_nz_NII*ma_nz_PSF).sum() / (ma_nz_PSF**2).sum()
 
-        # calculate central flux line ratios for BPT diagrams
+        log_halpha_cf = np.log10(halpha_cf)
+        log_hbeta_cf = np.log10(hbeta_cf)
+        log_SII_cf = np.log10(SII_cf)
+        log_OI_cf= np.log10(OI_cf)
+        log_OIII_cf= np.log10(OIII_cf)
+        log_NII_cf = np.log10(NII_cf)
+        
         log_SII_Ha_cf = np.log10(SII_cf/halpha_cf)
         log_OI_Ha_cf= np.log10(OI_cf/halpha_cf)
         log_OIII_Hb_cf= np.log10(OIII_cf/hbeta_cf)
         log_NII_Ha_cf = np.log10(NII_cf/halpha_cf)
 
-        return i, halpha_cf, hbeta_cf, SII_cf, OI_cf, OIII_cf, NII_cf, log_SII_Ha_cf, log_OI_Ha_cf, log_OIII_Hb_cf, log_NII_Ha_cf
+        return i, halpha_cf, hbeta_cf, SII_cf, OI_cf, OIII_cf, NII_cf, log_halpha_cf, log_hbeta_cf, log_SII_cf, log_OI_cf, log_OIII_cf, log_NII_cf, log_SII_Ha_cf, log_OI_Ha_cf, log_OIII_Hb_cf, log_NII_Ha_cf, SII_variance, OI_variance, OIII_variance, NII_variance, SII_sigma, OI_sigma, OIII_sigma, NII_sigma
 
 # Measure central flux for each good galaxy
-
 starttime = time.time()
 centralflux_data = []
 
 for i in MNSA_plateifu[pipe3D_index]:
     centralflux_data.append(central_flux(i))
-
+    
 endtime = time.time()
 elapsedtime = endtime - starttime
-
+    
 # make centralflux_data a structured ndarray
 cf_dtype = ([('plateifu', np.compat.unicode, 15),('halpha_cf', np.float64), ('hbeta_cf', np.float64),
              ('SII_cf', np.float64), ('OI_cf', np.float64),('OIII_cf', np.float64),
-             ('NII_cf', np.float64), ('log_SII_Ha_cf', np.float64), ('log_OI_Ha_cf', np.float64),
-             ('log_OIII_Hb_cf', np.float64),('log_NII_Ha_cf', np.float64)])
+             ('NII_cf', np.float64), ('log_halpha_cf', np.float64),('log_hbeta_cf', np.float64),
+             ('log_SII_cf', np.float64), ('log_OI_cf', np.float64), ('log_OIII_cf', np.float64), 
+             ('log_NII_cf', np.float64), ('log_SII_Ha_cf', np.float64), ('log_OI_Ha_cf', np.float64),
+             ('log_OIII_Hb_cf', np.float64), ('log_NII_Ha_cf', np.float64), ('SII_variance', np.float64), 
+             ('OI_variance', np.float64),('OIII_variance', np.float64), ('NII_variance', np.float64),
+             ('SII_sigma', np.float64),('OI_sigma', np.float64),('OIII_sigma', np.float64),
+             ('NII_sigma', np.float64)])
 
 centralflux_data = np.array(centralflux_data, dtype = cf_dtype)
 
@@ -175,8 +219,6 @@ def starformation_NII(log_NII_Ha):
     """Composite galaxy and LINER classification line for log([NII]/Ha)"""
     return 0.61 / (log_NII_Ha - 0.05) + 1.3
 
-# plot diagrams
-
 #log([OIII]/H-beta)/([SII]/H-alpha)
 plt.figure(figsize = (5,5))
 plt.scatter(centralflux_data['log_SII_Ha_cf'], centralflux_data['log_OIII_Hb_cf'],  s = 50, alpha = 0.3, marker ='.', linestyle = 'None')
@@ -193,7 +235,7 @@ plt.xlim(-1.5,1.0)
 plt.ylim(-1.0,1.5)
 plt.tight_layout()
 plt.minorticks_on()
-plt.savefig('/uufs/chpc.utah.edu/common/home/u6044257/Desktop/BPT_SII_Ha_OIII_Hb.png', overwrite = True)
+#plt.savefig('/uufs/chpc.utah.edu/common/home/u6044257/Desktop/BPT_SII_Ha_OIII_Hb.png', overwrite = True)
 plt.show()
 
 #log([OIII]/H-beta) & ([OI]/H-alpha)
@@ -211,7 +253,7 @@ plt.xlim(-2.5,0.5)
 plt.ylim(-1.0,1.5)
 plt.tight_layout()
 plt.minorticks_on()
-plt.savefig('/uufs/chpc.utah.edu/common/home/u6044257/Desktop/BPT_OI_Ha_OIII_Hb.png', overwrite = True)
+#plt.savefig('/uufs/chpc.utah.edu/common/home/u6044257/Desktop/BPT_OI_Ha_OIII_Hb.png', overwrite = True)
 plt.show()
 
 #log([OIII]/H-beta) & ([NII]/H-alpha)
@@ -230,17 +272,17 @@ plt.xlim(-2.0,1.0)
 plt.ylim(-1.0,1.5)
 plt.tight_layout()
 plt.minorticks_on()
-plt.savefig('/uufs/chpc.utah.edu/common/home/u6044257/Desktop/BPT_NII_Ha_OIII_Hb.png', overwrite = True)
+#plt.savefig('/uufs/chpc.utah.edu/common/home/u6044257/Desktop/BPT_NII_Ha_OIII_Hb.png', overwrite = True)
 plt.show()
 
 print('Elapsed time is:', time.strftime("%Hh%Mm%Ss", time.gmtime(elapsedtime)))
 
-# now let's check our central flux values with those calculated from Pipe3D
+# Now let's check our central flux values with those calculated from Pipe3D
 
 # galaxy properties we are interested in
 pipe3D_log_SII_Ha_cf = pipe3D_galproperties['log_SII_Ha_cen'][:]
 pipe3D_log_OI_Ha_cf = pipe3D_galproperties['log_OI_Ha_cen'][:]
-pipe3D_log_OIII_Hb_cf= pipe3D_galproperties['log_OIII_Hb_cen'][:]
+pipe3D_log_OIII_Hb_cf  = pipe3D_galproperties['log_OIII_Hb_cen'][:]
 pipe3D_log_NII_Ha_cf = pipe3D_galproperties['log_NII_Ha_cen'][:]
 
 pipe3D_log_SII_Ha_cf_err = pipe3D_galproperties['e_log_SII_Ha_cen'][:]
@@ -248,24 +290,55 @@ pipe3D_log_OI_Ha_cf_err = pipe3D_galproperties['e_log_OI_Ha_cen'][:]
 pipe3D_log_OIII_Hb_cf_err = pipe3D_galproperties['e_log_OIII_Hb_cen'][:]
 pipe3D_log_NII_Ha_cf_err = pipe3D_galproperties['e_log_NII_Ha_cen'][:]
 
-# percent error between calculated log values and Pipe3D log values of central flux (in percent)
-err_1 = np.abs((centralflux_data['log_SII_Ha_cf'] - pipe3D_log_SII_Ha_cf) / pipe3D_log_SII_Ha_cf) * 100 
-err_2 = np.abs((centralflux_data['log_OI_Ha_cf'] - pipe3D_log_OI_Ha_cf) / pipe3D_log_OI_Ha_cf) * 100
-err_3 = np.abs((centralflux_data['log_OIII_Hb_cf'] - pipe3D_log_OIII_Hb_cf) / pipe3D_log_OIII_Hb_cf) * 100
-err_4 = np.abs((centralflux_data['log_NII_Ha_cf'] - pipe3D_log_NII_Ha_cf) / pipe3D_log_NII_Ha_cf) * 100
+delta_1 = centralflux_data['log_SII_Ha_cf'] -  pipe3D_log_SII_Ha_cf
+delta_2 = centralflux_data['log_OI_Ha_cf'] - pipe3D_log_OI_Ha_cf
+delta_3 = centralflux_data['log_OIII_Hb_cf'] - pipe3D_log_OIII_Hb_cf
+delta_4 = centralflux_data['log_NII_Ha_cf'] - pipe3D_log_NII_Ha_cf
 
-print(centralflux_data['log_SII_Ha_cf'])
-print(centralflux_data['log_OI_Ha_cf'])
-print(centralflux_data['log_OIII_Hb_cf'])
-print(centralflux_data['log_NII_Ha_cf'] )
+err_1 = delta_1/centralflux_data['SII_sigma'] 
+err_2 = delta_2/centralflux_data['OI_sigma'] 
+err_3 = delta_3/centralflux_data['OIII_sigma'] 
+err_4 = delta_4/centralflux_data['NII_sigma'] 
 
-print(pipe3D_log_SII_Ha_cf)
-print(pipe3D_log_OI_Ha_cf)
-print(pipe3D_log_OIII_Hb_cf)
-print(pipe3D_log_NII_Ha_cf)
-    
-print('log_SII_Ha_cf error:',  err_1)
-print('log_OI_Ha_cf error:', err_2)
-print('log_OIII_Hb_cf error:', err_3)
-print('log_NII_Ha_cf error:', err_4)
+# plot of errors
+plt.figure(figsize = (5,5))
+plt.plot(centralflux_data['log_SII_cf'], err_1, 'o')
 
+plt.title('Error in Central Flux of SII Doublet')
+plt.xlabel(r'log([SII]')
+plt.ylabel(r'${\Delta}$log([SII]/H${\alpha}$)')
+plt.tight_layout()
+plt.grid()
+plt.minorticks_on()
+
+plt.figure(figsize = (5,5))
+plt.plot(centralflux_data['log_OI_cf'], err_2, 'o')
+
+plt.title('Error in Central Flux of OI')
+plt.xlabel(r'log([OI]')
+plt.ylabel(r'${\Delta}$log([OI]/H${\alpha}$)')
+plt.tight_layout()
+plt.grid()
+plt.minorticks_on()
+
+plt.figure(figsize = (5,5))
+plt.plot(centralflux_data['log_OIII_cf'], err_3, 'o')
+
+plt.title('Error in Central Flux of OIII')
+plt.xlabel(r'log([OIII]')
+plt.ylabel(r'${\Delta}$log([OIII]/H${\beta}$)')
+plt.tight_layout()
+plt.grid()
+plt.minorticks_on()
+
+plt.figure(figsize = (5,5))
+plt.plot(centralflux_data['log_NII_cf'], err_4, 'o')
+
+plt.title('Error in Central Flux of NII')
+plt.xlabel(r'log([NII]')
+plt.ylabel(r'${\Delta}$log([NII]/H${\alpha}$)')
+plt.tight_layout()
+plt.grid()
+plt.minorticks_on()
+
+plt.show()
